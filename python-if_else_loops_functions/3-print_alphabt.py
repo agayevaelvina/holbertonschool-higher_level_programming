9e2eea29f2487598d4c7ef758d22136adb578615
agayevaelvina <agayevaelvina>
2025-06-12 14:48:08 +0000

    x

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-print("".join("{}".format(chr(i)) for i in range(97, 123) if chr(i) not in ('e', 'q')), end="")
+print(
+    "".join(
+        "{}".format(chr(i)) for i in range(97, 123)
+        if chr(i) not in ('e', 'q')
+    ),
+    end=""
+)
