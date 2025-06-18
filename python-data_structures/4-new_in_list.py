eaf90837a077706e988b1c77a0e4623b8934700b
agayevaelvina <elvinaagyeva@gmail.com>
2025-06-18 13:42:47 +0000

    x

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    if my_list:
-        for i in range(len(my_list) - 1, -1, -1):
-            print("{:d}".format(my_list[i]))
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list[:]
+    new_list = my_list[:]
+    new_list[idx] = element
+    return new_list
