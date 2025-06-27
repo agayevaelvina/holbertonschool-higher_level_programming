3a84f8b8974ed704965e2a28fdf6559f8c2a4f16
agayevaelvina <elvinaagyeva@gmail.com>
2025-06-27 12:18:21 +0000

    x

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class with width and height properties."""
-
-
 class Rectangle:
-    """Represent a rectangle with width and height."""
-
     def __init__(self, width=0, height=0):
-        """Initialize the rectangle with optional width and height."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Get the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Set the width of the rectangle."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -26,26 +18,13 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Set the height of the rectangle."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-
-# Test code for exception handling
-try:
-    my_rectangle = Rectangle(2, -3)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    my_rectangle = Rectangle(-2, 3)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
