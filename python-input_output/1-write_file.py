b8b729a55f6c11c9d32f4b9a0e6e7cefcef3f311
Your Name <elv>
2025-06-28 11:14:18 +0000

    x

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def write_file(filename="", text=""):
+    """Writes a string to a UTF-8 text file and returns the number of characters written."""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
