b5b52857039b80e9058f0badd4170d9153eef9b0
Your Name <elv>
2025-06-28 11:35:42 +0000

    x

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+Script that adds all arguments to a Python list,
+then saves them to a file using JSON format.
+
+Usage: ./7-add_item.py <item1> <item2> ...
+"""
+
+import sys
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
+
+filename = "add_item.json"
+
+if __name__ == "__main__":
+    try:
+        items = load_from_json_file(filename)
+    except FileNotFoundError:
+        items = []
+
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, filename)
