cc5ce452d111d4056fac90ee5e0302f9222fc1b3
Your Name <elv>
2025-06-28 11:13:04 +0000

    x

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Reads a UTF-8 text file and prints its content to stdout."""
+
+
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its content to stdout."""
     with open(filename, encoding="utf-8") as f:
