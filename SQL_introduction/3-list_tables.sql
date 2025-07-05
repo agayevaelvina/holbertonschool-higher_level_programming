905a9110d5276ce19ab222eb20ac0a986bfd35e8
Your Name <you@example.com>
2025-07-05 19:48:16 +0000

    x

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,5 +1,4 @@
-#!/bin/bash
-# x
+--  x
 if [ -z "$1" ]; then
   echo "Usage: $0 <database_name>"
   exit 1
@@ -7,5 +6,4 @@ fi
 
 DB_NAME="$1"
 
-# List all tables in the specified MySQL database
 mysql "$DB_NAME" -e "SHOW TABLES;"
