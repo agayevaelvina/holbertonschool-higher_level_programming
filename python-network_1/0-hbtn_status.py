cf747fe095daa4ee59bcd565de48d14b267310f3
Your Name <you@example.com>
2025-07-12 13:38:38 +0000

    x

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,13 +5,13 @@ from urllib import request
 
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
-    req = request.Request(url, headers={"User-Agent": "Mozilla/5.0"})
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    req = request.Request(url, headers=headers)
 
     with request.urlopen(req) as response:
         body = response.read()
-        utf8_content = body.decode('utf-8')
-
-    print("Body response:")
-    print("\t- type:", type(body))
-    print("\t- content:", body)
-    print("\t- utf8 content:", utf8_content)
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode("utf-8"))
