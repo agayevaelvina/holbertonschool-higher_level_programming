abdc45b3a13aeba748ab63337f6119e511516479
Your Name <you@example.com>
2025-07-12 13:34:46 +0000

    x

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""Fetches a URL"""
+"""Fetches https://intranet.hbtn.io/status"""
+
 from urllib import request
 
 if __name__ == "__main__":
-    """Makes code executable when it is directly run"""
     url = "https://intranet.hbtn.io/status"
-    req = request.Request(url)
-    req.add_header('cfclearance', 'true')
+    req = request.Request(url, headers={"User-Agent": "Mozilla/5.0"})
 
     with request.urlopen(req) as response:
-        """Opens the url and reads the contents"""
         body = response.read()
         utf8_content = body.decode('utf-8')
 
-    """Prints out the desired output"""
     print("Body response:")
     print("\t- type:", type(body))
     print("\t- content:", body)
