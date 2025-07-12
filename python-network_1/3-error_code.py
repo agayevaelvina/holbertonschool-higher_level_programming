403d83f94e4165268820a634705e6ec3a50cc810
Your Name <you@example.com>
2025-07-12 18:44:29 +0000

    x

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Sends a request and handles HTTP errors"""
-
+""" Send a request and handle exception"""
 import sys
 from urllib import request, error
 
 if __name__ == "__main__":
+    """Ensure the code is executed when direclty run"""
     url = sys.argv[1]
+    req = request.Request(url)
+    req.add_header("cfclearance", 'true')
 
     try:
-        with request.urlopen(url) as response:
-            body = response.read().decode("utf-8")
+        with request.urlopen(req) as response:
+            body = response.read().decode('utf-8')
             print(body)
     except error.HTTPError as e:
         print("Error code:", e.code)
